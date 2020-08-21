76c40c5e6aeb8f1aae846d860c5da5b0c99c4b5e
João Eira <simbyotic@gmail.com>
2020-08-21 20:12:54 +0100

    initial implementation of getting all .img from the card, hacky usage of Promise.all that I don't totally understand [Promise.all fires up when all promises have been completed so in a sense I get it, I want to send the image array only after all promises are done with, but I can't say I came up with this solution because I understood the need for it. ALso theres a bug where the first two objects are empty for some reason]

--- app.js
+++ app.js
@@ -5,59 +5,6 @@ const app = express();
 
 const ANKICONNECT = 'http://127.0.0.1:8765';
 
-
-
-// fetch(ANKICONNECT,getCurrentCard)
-//     .then(res => {
-//         res.json().then(data => {
-//             console.log([data.question,data.answer])})
-//     });
-
-// const getDueCards = async () => {
-//     const options = {
-//         method: 'POST',
-//         body: JSON.stringify({
-//             action: 'findCards',
-//             params: {query: "is:due"}
-//         })
-//     }
-//     try{
-//         const fetchedDeckCards = await fetch(ANKICONNECT,options);
-//         const deckCards = await fetchedDeckCards.json();
-//         return deckCards;
-//     } catch(error){console.error(error);}
-// }
-
-
-
-// const getCardInfo = async (card) => {
-//     const options = {
-//         method: 'POST',
-//         body: JSON.stringify({
-//             action: 'cardsInfo',
-//             params: {cards: [card]}
-//         })
-//     };
-
-
-//     try{
-//         const fetchedCardInfo = await fetch(ANKICONNECT,options);
-//         const cardInfo = await fetchedCardInfo.json();
-//         return cardInfo;
-//     }catch(error){console.error(error);}
-// }
-
-
-
-
-// const getCardQuestionAndAnswer = async (card) => {
-//     const cardInfo = await getCardInfo(card);
-//     return {
-//         question: cardInfo[0].question,
-//         answer: cardInfo[0].answer
-//     }
-// }
-
 const getCurrentCardInfo = async () => {
     // Returns card object when card is active for review in GUI
     // NOTE: because startReview() is invoked, the review session will be initialized
@@ -75,7 +22,6 @@ const getCurrentCardInfo = async () => {
 }
 
 const returnRenderElements = async (cardInfo) => {
-    //TODO: Return images from getImages() (need to decode ? how)
     //TODO: Substitute it in place, do it per array (first element gets first image from getImages()))
     const cardImage = await getImages(cardInfo);
 
@@ -105,43 +51,45 @@ const startReview = async() => {
     } catch(error){throw new Error(error)}
 }
 
-// TODO: Create function to return media files
-// TODO: Update returnRenderElements() so that it retrieves media elements (if there are any)
-// NOTE: One thing to keep in mind is that doing id.innerHTML = card.question won't work because
-//       You have to get the image from somewhere else...
-
 
 const getImages = async (cardInfo) => {
-
+    //TODO: Refactor function to return array of image data
     const domAnswer = new jsdom.JSDOM(cardInfo.answer);
-    const hasImage = domAnswer.window.document.getElementsByTagName('img').length !== 0;
 
-    const options = {
-        method: 'POST',
-        body: JSON.stringify({
-            action: 'retrieveMediaFile',
-            params: {
-                filename: domAnswer.window.document.getElementsByTagName('img')[0].getAttribute('src').split('/').pop()} //TODO What about multiple images, and svgs (which I think is what Image Clozes uses)?
-        })
-    };
+    const domImages = Object.values(domAnswer.window.document.getElementsByTagName('img'));
+    //TODO: What about what isnt img?
+    //BUG: Why are the first two objects in the array empty? 
     try{
-        const fetchedResponse = await fetch(ANKICONNECT,options);
-        const fetchedJSON = await fetchedResponse.json();
-        return fetchedJSON;
+
+        let imageArray;
+
+        await Promise.all(
+
+            imageArray = domImages.map(async item  =>  {
+
+            const options = {
+                method: 'POST',
+                body: JSON.stringify({
+                    action: 'retrieveMediaFile',
+                    params: {
+                        filename: item.getAttribute('src')} 
+                })
+            };
+
+
+            const fetchedResponse = await fetch(ANKICONNECT,options);
+            const fetchedJSON = await fetchedResponse.json();
+            imageArray.push(fetchedJSON);
+
+            })
+        );
+        return imageArray;
 
     } catch(error){console.error(error);}
-}
+    }
 
-getCurrentCardInfo().then(cardInfo => {
-    getImages(cardInfo).then(html => console.log())
-});
-
-// getCurrentCardInfo().then(cardInfo=> {
-//     getImages(cardInfo);
-// })
 
 module.exports = {
     getCurrentCardInfo: getCurrentCardInfo,
     returnRenderElements: returnRenderElements
 }
-
